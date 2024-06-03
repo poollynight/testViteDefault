ff6f604976077668d78d0ba45a6b97d052bc6d21
poollynight <chienthang410@gmail.com>
2024-06-03 15:41:14 +0700

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,152 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <v-btn>123</v-btn>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <link
+    href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+    rel="stylesheet"
+  />
+  <v-app>
+    <!-- Header -->
+    <v-container-fluid class="border">
+      <!-- Top header -->
+      <v-toolbar color="red-darken-4" height="40">
+        <v-container>
+          <v-row class="d-flex" align="center" justify="center">
+            <v-col class="text-center">Nước hoa cho mọi tầng lớp</v-col>
+          </v-row>
+        </v-container>
+      </v-toolbar>
+
+      <!-- Bottom header -->
+      <v-toolbar height="100" class="bg-white">
+        <v-row align="center">
+          <v-col
+            cols="3"
+            class="text-red-darken-4 h-100 d-inline-flex justify-center text-h5"
+          >
+            <h1>
+              <v-p
+                v-model="tab"
+                @click="tabSelected('Home')"
+                class="text-decoration-none text-weight-bold cursor-pointer"
+              >
+                Odour
+              </v-p>
+            </h1>
+          </v-col>
+          <v-col cols="6">
+            <v-tabs
+              class="d-flex justify-center"
+              v-model="tab"
+              align-tabs="center"
+              color="red"
+            >
+              <v-tab :value="Home" @click="tabSelected('Home')"
+                ><h3 class="tab">Home</h3></v-tab
+              >
+              <v-tab :value="Shop" @click="tabSelected('Shop')"
+                ><h3 class="tab">Shop</h3></v-tab
+              >
+              <v-tab :value="About" @click="tabSelected('About')"
+                ><h3 class="tab">About</h3></v-tab
+              >
+              <v-tab :value="Contact" @click="tabSelected('Contact')"
+                ><h3 class="tab">Contact</h3></v-tab
+              >
+            </v-tabs>
+          </v-col>
+          <v-col
+            cols="1"
+            color="success"
+            class="col-3 d-flex justify-space-between"
+          >
+            <p></p>
+            <a href=""><i class="fas fa-shopping-cart"></i></a>
+            <a href=""><i class="fas fa-user"></i></a>
+          </v-col>
+        </v-row>
+      </v-toolbar>
+    </v-container-fluid>
+    <v-main>
+      <Home v-if="HomeSelected" />
+      <Shop v-if="ShopSelected" @load-product="loadProduct"></Shop>
+      <About v-if="AboutSelected"></About>
+      <Contact v-if="ContactSelected"></Contact>
+      <Product
+        v-if="componentName == 'Product'"
+        :item="componentParam"
+      ></Product>
+    </v-main>
+    <Footer></Footer>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import Product from "./components/Product.vue";
+import Shop from "./components/Shop.vue";
+export default {
+  name: "app",
+  components: {
+    Shop,
+    Product,
+  },
+  
+  data: () => ({
+    tab: null,
+    HomeSelected: true,
+    ShopSelected: false,
+    AboutSelected: false,
+    ContactSelected: false,
+    componentName: null,
+    componentParam: null,
+  }),
+  methods: {
+    tabSelected(tab) {
+      localStorage.setItem("tab", tab);
+      this.switchTab();
+    },
+    switchTab() {
+      var tab = localStorage.getItem("tab");
+      if (tab == "Home") {
+        this.HomeSelected = true;
+        this.ShopSelected = false;
+        this.AboutSelected = false;
+        this.ContactSelected = false;
+        this.componentName = null;
+      } else if (tab == "Shop") {
+        this.ShopSelected = true;
+        this.HomeSelected = false;
+        this.AboutSelected = false;
+        this.ContactSelected = false;
+        this.componentName = null;
+      } else if (tab == "About") {
+        this.AboutSelected = true;
+        this.HomeSelected = false;
+        this.ShopSelected = false;
+        this.ContactSelected = false;
+        this.componentName = null;
+      } else if (tab == "Contact") {
+        this.ContactSelected = true;
+        this.HomeSelected = false;
+        this.ShopSelected = false;
+        this.AboutSelected = false;
+        this.componentName = null;
+      }
+    },
+
+    loadProduct(item) {
+      this.componentName = "Product";
+      this.componentParam = item;
+      this.ShopSelected = false;
+    },
+
+    
+  },
+  mounted() {
+    this.switchTab();
+  },
+};
+</script>
+<style>
+.tab {
+  font-size: 1em;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-card color="grey-lighten-4">
+    <v-container>
+      <v-row justify="center" text-align="center">
+        <v-col cols="5" align="center">
+          <v-img height="400" cover src="/src/assets/cate-men.jpg"></v-img>
+          <v-sheet
+            class="mx-auto mt-5"
+            max-width="500"
+            max-height="150"
+            color="grey-lighten-4"
+          >
+            <v-slide-group
+              v-model="model"
+              color="grey"
+              class="pa-4"
+              center-active
+              show-arrows
+            >
+              <v-slide-group-item
+                v-for="n in item.image.length"
+                :key="n"
+                v-slot="{ isSelected, toggle }"
+              >
+                <v-card
+                  :color="isSelected ? 'grey-lighten-2' : 'grey-lighten-1'"
+                  class="ma-4"
+                  height="100"
+                  width="100"
+                  @click="toggle"
+                >
+                  <v-img
+                    height="200"
+                    aspect-ratio="16/9"
+                    cover
+                    src="/src/assets/cate-men.jpg"
+                  ></v-img>
+                </v-card>
+              </v-slide-group-item>
+            </v-slide-group>
+          </v-sheet>
+        </v-col>
+        <v-col cols="7" align="center">
+          <v-card class="text-left pl-5">
+            <div>
+              <p class="ProName">{{ item.name }}</p>
+              <p class="price mt-3 text-h6">{{ item.price }}</p>
+            </div>
+            <div>
+              <p class="text-h6">Description:</p>
+              <p>{{ item.description }}</p>
+            </div>
+            <div>
+              <p class="text-h6">Nhóm hương:</p>
+              <p>{{ item.category }}</p>
+            </div>
+            <div>
+              <p class="text-h6">Tầng hương:</p>
+              <p class="text-subtitle-1">
+                Tầng đầu: {{ item.category }}
+                <br />
+                Tầng giữa: {{ item.category }}
+                <br />
+                Tầng cuối: {{ item.category }}
+              </p>
+            </div>
+            <div>
+              <p class="text-h6">Độ lưu hương:</p>
+              <p class="text-subtitle-1">
+                Trên da: {{ item.category }}
+                <br />
+                Trên vải: {{ item.category }}
+              </p>
+            </div>
+            <div class="d-flex">
+              <p class="text-h5 mr-4">Số lượng</p>
+              <v-row>
+                <v-col cols="2">
+                  <v-btn
+                    class="bg-red-darken-4 mr-5"
+                    @click="decreaseQuantity()"
+                  >
+                    <v-icon icon="mdi-minus"></v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col cols="2">
+                  <v-input disabled class="text-center quantityInput">
+                    {{ quantity }}
+                  </v-input>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn class="bg-red-darken-4" @click="increaseQuantity()">
+                    <v-icon icon="mdi-plus"></v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+            <div class="d-flex flex-row">
+              <v-btn class="bg-red-darken-4 mr-6 w-25">Mua ngay</v-btn>
+              <v-btn class="bg-red-darken-4" @click="addToCart"
+                >Thêm vào giỏ hàng</v-btn
+              >
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<style>
+.text-left div {
+  margin-bottom: 1rem;
+}
+.ProName {
+  font-size: xx-large;
+}
+.quantityInput {
+  height: 2em;
+}
+</style>
+
+<script>
+// import { useCookies } from "vue-cookies";
+export default {
+  // setup() {
+  //   const { cookies } = useCookies();
+
+  //   const setCookie = () => {
+  //     cookies.set("myCookie", "myValue", 30 * 24 * 60 * 60); // Set cookie for 30 days
+  //     console.log(123);
+  //   };
+
+  //   return {
+  //     setCookie,
+  //   };
+  // },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    model: null,
+    quantity: 1,
+  }),
+  methods: {
+    increaseQuantity() {
+      this.quantity += 1;
+    },
+    decreaseQuantity() {
+      this.quantity -= 1;
+      if (this.quantity < 1) this.quantity = 1;
+    },
+
+    addToCart() {
+      // this.$cookie.set("keyName", keyValue, "expiring time");
+      // setCookie('cookie','my cookie',30 )
+      document.cookie = "keyName" + "=" + [{name: "my cookie", value: "my cookie"},{name:"my cookie",value:"my cookie"}];
+      console.log(document.cookie);
+    },
+  },
+};
+</script>
