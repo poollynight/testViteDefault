86a59c148f4127065ce5233be0710685e23b8bfc
poollynight <chienthang410@gmail.com>
2024-06-03 15:39:04 +0700

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <v-btn>123</v-btn>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
